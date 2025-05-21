8ef96d7d8ecb6a6a98705e67f54d6bce714ede6d
Kuwata-bit <hideya0813@gmail.com>
2025-05-21 16:05:28 +0900

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ここにGoogleスプレッドシートから発行したCSVのURLを貼り付けてください
-const SPREADSHEET_URL = 'YOUR_PUBLISHED_GOOGLE_SHEET_CSV_URL';
+const SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQF1H6eCIEv3AJ23kdwKsYiO6X8V2XgTpHxLYNJE9CkympIW3ZTJm9q4qzQrWKAzOUwFR1fwrwX-lGv/pubhtml?gid=0&single=true';
 // 時刻が記載されている列のインデックス (0から始まる。例: 最初の列なら0)
 const TIME_COLUMN_INDEX = 0; // IMPORTHTMLで取得したデータに合わせて調整してください
 
